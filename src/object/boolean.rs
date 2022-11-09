3e58c81a1b7bfc5d3647773199cb8d250b16f2ea
Henning Meyer <tutmann@gmail.com>
2022-11-09 08:20:22 +0100

    refactor booleans. derive all from union

--- src/object/boolean.rs
+++ src/object/boolean.rs
@@ -1,130 +1,77 @@
 use crate::object::Object;
 use std::collections::HashSet;
 
-trait BinaryBoolean {
-    fn get_children(&self) -> &[Box<dyn Object>];
-    fn get_op(&self) -> &'static str;
-    fn get_fn_name(&self) -> &'static str;
-    fn get_full_fn_name(&self) -> String {
-        format!(
-            "{}{}{}",
-            self.get_fn_name(),
-            if self.get_smoothness() > 0. {
-                "Smooth"
-            } else {
-                ""
-            },
-            self.get_children().len()
-        )
-    }
-    fn get_smoothness(&self) -> f32;
+enum UnionKind {
+    Default,
+    Polynomial, // Supports only two objects.
 }
 
-impl<T> Object for T
-where
-    T: BinaryBoolean,
-{
-    fn static_code(&self) -> HashSet<String> {
-        let mut code_set = HashSet::new();
-        for c in self.get_children() {
-            code_set.extend(c.static_code());
+impl UnionKind {
+    fn function_name(&self, num_objects: usize) -> String {
+        match self {
+            UnionKind::Default => format!("opMin{}", num_objects),
+            UnionKind::Polynomial => format!("opSmoothMinPolynomial{}", num_objects),
         }
-        if self.get_smoothness() > 0. {
-            code_set.insert(smooth_binary_function(
-                self.get_children().len(),
-                &self.get_full_fn_name(),
-                self.get_op(),
-            ));
-        } else {
-            code_set.insert(binary_function(
-                self.get_children().len(),
-                &self.get_full_fn_name(),
-                self.get_op(),
-            ));
-        }
-        code_set
     }
-    fn expression(&self, p: &str) -> String {
-        if self.get_children().len() == 1 {
-            return self.get_children()[0].expression(p);
+    fn make_params(num_objects: usize) -> Vec<String> {
+        (0..num_objects).map(|i| format!("d{}", i)).collect()
+    }
+    fn make_function(&self, num_objects: usize) -> String {
+        if num_objects < 2 {
+            panic!("Union function needs at least to objects.")
         }
-        let child_exps = (self.get_children().iter())
-            .map(|c| c.expression(p))
-            .collect::<Vec<_>>()
-            .join(", ");
-        let smoothness_exp = if self.get_smoothness() > 0. {
-            format!(", {:.8}", self.get_smoothness())
-        } else {
-            String::new()
-        };
-        return format!(
-            "{}({}{})",
-            self.get_full_fn_name(),
-            child_exps,
-            smoothness_exp
-        );
+        match self {
+            UnionKind::Default => make_default_min_function(
+                &self.function_name(num_objects),
+                &UnionKind::make_params(num_objects),
+            ),
+            UnionKind::Polynomial => {
+                make_polynomial_min_function(&self.function_name(num_objects), num_objects)
+            }
+        }
     }
 }
 
-fn binary_function(num_objects: usize, full_fn_name: &str, op: &str) -> String {
-    if num_objects == 0 {
-        return String::new();
-    }
-    let params = (0..num_objects)
-        .map(|i| format!("float d{}", i))
+fn make_default_min_function(function_name: &str, params: &[String]) -> String {
+    let expr_begin = params[0..params.len() - 1]
+        .iter()
+        .map(|p| format!("min({}", p))
         .collect::<Vec<_>>()
         .join(", ");
-    let expr_begin = (0..num_objects - 1)
-        .map(|i| format!("{}(d{}", op, i))
-        .collect::<Vec<_>>()
-        .join(", ");
-    let expr_end = (0..num_objects - 1)
-        .map(|_| ")")
-        .collect::<Vec<_>>()
-        .join("");
+    let expr_end = String::from_utf8(vec![b')'; params.len() - 1]).unwrap();
     format!(
         "
-    float {}({}) {{
-    return {expr_begin}{expr_mid}{expr_end};
-    }}
-    ",
-        full_fn_name,
-        params,
+float {}(float {}) {{
+return {expr_begin}, {last_param}{expr_end};
+}}
+",
+        function_name,
+        params.join(", float "),
         expr_begin = expr_begin,
-        expr_mid = format!(", d{}", num_objects - 1),
+        last_param = params[params.len() - 1],
         expr_end = expr_end
     )
 }
 
-fn smooth_binary_function(num_objects: usize, full_fn_name: &str, op: &str) -> String {
-    if num_objects == 0 {
-        return String::new();
+fn make_polynomial_min_function(function_name: &str, num_objects: usize) -> String {
+    if num_objects != 2 {
+        panic!("Polynomial min required exactly two arguments.");
     }
-    if num_objects == 2 {
-        let sgn = match op {
-            "min" => '-',
-            "max" => '+',
-            _ => unimplemented!(),
-        };
-        format!(
-            "
+    format!(
+        "
 float {name}(float d1, float d2, float k) {{
     float h = max(k-abs(d1-d2),0.0);
-    return {op}(d1, d2) {sgn} h*h*0.25/k;    
+    return min(d1, d2) - h*h*0.25/k;    
 }}
-        ",
-            name = full_fn_name,
-            op = op,
-            sgn = sgn
-        )
-    } else {
-        unimplemented!()
-    }
+",
+        name = function_name
+    )
 }
 
 pub struct Union {
     children: Vec<Box<dyn Object>>,
     smoothness: f32,
+    kind: UnionKind,
 }
 
 impl Union {
@@ -135,66 +82,47 @@ impl Union {
         children: Vec<Box<dyn Object>>,
         smoothness: f32,
     ) -> Result<Union, String> {
-        if children.len() == 0 {
-            return Err("Empty children for Union.".to_string());
+        if children.len() < 2 {
+            return Err("Union requires at least 2 children.".to_string());
         }
+        let kind = if smoothness == 0.0 {
+            UnionKind::Default
+        } else {
+            UnionKind::Polynomial
+        };
         Ok(Union {
             children,
             smoothness,
+            kind,
         })
     }
 }
 
-impl BinaryBoolean for Union {
-    fn get_children(&self) -> &[Box<dyn Object>] {
-        &self.children
-    }
-    fn get_op(&self) -> &'static str {
-        "min"
-    }
-    fn get_fn_name(&self) -> &'static str {
-        "opUnion"
-    }
-    fn get_smoothness(&self) -> f32 {
-        self.smoothness
-    }
-}
-
-pub struct Intersection {
-    children: Vec<Box<dyn Object>>,
-    smoothness: f32,
-}
-
-impl Intersection {
-    pub fn new(children: Vec<Box<dyn Object>>) -> Result<Intersection, String> {
-        Intersection::new_with_smoothness(children, 0.)
-    }
-    pub fn new_with_smoothness(
-        children: Vec<Box<dyn Object>>,
-        smoothness: f32,
-    ) -> Result<Intersection, String> {
-        if children.len() == 0 {
-            return Err("Empty children for Intersection.".to_string());
+impl Object for Union {
+    fn static_code(&self) -> HashSet<String> {
+        let mut code_set = HashSet::new();
+        for child in &self.children {
+            code_set.extend(child.static_code());
         }
-        Ok(Intersection {
-            children,
-            smoothness,
-        })
+        code_set.insert(self.kind.make_function(self.children.len()));
+        code_set
     }
-}
-
-impl BinaryBoolean for Intersection {
-    fn get_children(&self) -> &[Box<dyn Object>] {
-        &self.children
-    }
-    fn get_op(&self) -> &'static str {
-        "max"
-    }
-    fn get_fn_name(&self) -> &'static str {
-        "opIntersection"
-    }
-    fn get_smoothness(&self) -> f32 {
-        self.smoothness
+    fn expression(&self, p: &str) -> String {
+        let child_exps = (self.children.iter())
+            .map(|c| c.expression(p))
+            .collect::<Vec<_>>()
+            .join(", ");
+        let smoothness_exp = if self.smoothness > 0. {
+            format!(", {:.8}", self.smoothness)
+        } else {
+            String::new()
+        };
+        return format!(
+            "{}({}{})",
+            self.kind.function_name(self.children.len()),
+            child_exps,
+            smoothness_exp
+        );
     }
 }
 
@@ -203,6 +131,37 @@ struct Negation {
 }
 
 impl Object for Negation {
+    fn static_code(&self) -> HashSet<String> {
+        self.object.static_code()
+    }
+    fn expression(&self, p: &str) -> String {
+        format!("-({})", self.object.expression(p))
+    }
+}
+
+pub struct Intersection {
+    object: Union,
+}
+
+impl Intersection {
+    pub fn new(children: Vec<Box<dyn Object>>) -> Result<Intersection, String> {
+        Intersection::new_with_smoothness(children, 0.)
+    }
+    pub fn new_with_smoothness(
+        mut children: Vec<Box<dyn Object>>,
+        smoothness: f32,
+    ) -> Result<Intersection, String> {
+        let neg_children = children
+            .into_iter()
+            .map(|object| Box::new(Negation { object: object }) as Box<dyn Object>)
+            .collect();
+        Ok(Intersection {
+            object: Union::new_with_smoothness(neg_children, smoothness)?,
+        })
+    }
+}
+
+impl Object for Intersection {
     fn static_code(&self) -> HashSet<String> {
         self.object.static_code()
     }
@@ -222,7 +181,7 @@ impl Difference {
         smoothness: f32,
     ) -> Result<Intersection, String> {
         if children.len() == 0 {
-            return Err("Empty children for Difference.".to_string());
+            return Err("Difference requires at least one child.".to_string());
         }
         let mut new_children = vec![children.swap_remove(0)];
         while !children.is_empty() {
