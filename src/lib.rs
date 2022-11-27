c603d909b11d10f90ac4b5fa1c7320b3de1e2967
Henning Meyer <tutmann@gmail.com>
2022-11-27 10:56:29 +0100

    add basic meshing

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use crate::primitive::Primitive;
+use anyhow::{bail, Result};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 
@@ -6,6 +7,7 @@ use wasm_bindgen::JsCast;
 extern crate log;
 use log::Level;
 
+extern crate isosurface;
 extern crate nalgebra as na;
 
 mod primitive;
@@ -19,7 +21,7 @@ pub fn start() -> Result<(), JsValue> {
     console_log::init_with_level(Level::Info).unwrap();
     let window = web_sys::window().unwrap();
     let document = window.document().unwrap();
-    let canvas = document.get_element_by_id("canvas").unwrap();
+    let canvas = document.get_element_by_id("shader_canvas").unwrap();
     let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
     let canvas = render_canvas::RenderCanvas::new(canvas)?;
 
@@ -29,15 +31,31 @@ pub fn start() -> Result<(), JsValue> {
         };
         canvas.draw();
     };
-    let run_button = document.get_element_by_id("run").unwrap();
-    let run_button: web_sys::HtmlButtonElement =
-        run_button.dyn_into::<web_sys::HtmlButtonElement>()?;
+    let run_button = get_button(&document, "run").map_err(|e| e.to_string())?;
+    let mesh_button = get_button(&document, "mesh").map_err(|e| e.to_string())?;
     let script = document.get_element_by_id("program").unwrap();
     let script: web_sys::HtmlTextAreaElement = script.dyn_into::<web_sys::HtmlTextAreaElement>()?;
     let output = document.get_element_by_id("output").unwrap();
     let output: web_sys::HtmlTextAreaElement = output.dyn_into::<web_sys::HtmlTextAreaElement>()?;
     let engine = script_engine::RhaiScriptEngine::new();
-    _ = script_ui::ScriptUI::new(script, output, run_button, engine, new_object_callback)?;
+    _ = script_ui::ScriptUI::new(
+        script,
+        output,
+        run_button,
+        mesh_button,
+        engine,
+        new_object_callback,
+    )?;
 
     Ok(())
+}
+
+fn get_button(document: &web_sys::Document, name: &str) -> Result<web_sys::HtmlButtonElement> {
+    match document.get_element_by_id(name) {
+        Some(e) => match e.clone().dyn_into::<web_sys::HtmlButtonElement>() {
+            Ok(b) => Ok(b),
+            Err(_) => bail!("Cannot cast {:?} into HtmlButtonElement", e),
+        },
+        None => bail!("Did not find {}.", name),
+    }
 }
--- src/script_ui.rs
+++ src/script_ui.rs
@@ -1,42 +1,122 @@
 use super::script_engine::ScriptEngine;
 use super::Primitive;
+use std::cell::RefCell;
+use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::{HtmlButtonElement, HtmlTextAreaElement};
 
-pub struct ScriptUI {}
+pub struct ScriptUI {
+    primitive: Option<Box<dyn Primitive>>,
+}
 
 impl ScriptUI {
     pub fn new<E: ScriptEngine + 'static>(
         code_area: HtmlTextAreaElement,
         output_area: HtmlTextAreaElement,
         run_button: HtmlButtonElement,
+        mesh_button: HtmlButtonElement,
         mut engine: E,
         on_new_object_callback: impl Fn(&(dyn Primitive)) + 'static,
-    ) -> Result<ScriptUI, JsValue> {
-        {
-            let output_area = output_area.clone();
-            engine.on_print(move |s| {
-                let mut output = output_area.value();
-                output.push_str(s);
-                output.push('\n');
-                output_area.set_value(&output);
-            });
-        }
+    ) -> Result<Rc<RefCell<ScriptUI>>, JsValue> {
+        connect_output_to_engine(&output_area, &mut engine);
         let engine = engine;
-        {
-            let code_area = code_area.clone();
-            let closure = move |_event: web_sys::MouseEvent| match engine.eval(&code_area.value()) {
-                Ok(primitive) => on_new_object_callback(&*primitive),
-                Err(e) => info!("was err: {:?}", e),
-            };
-            // Run closure once to create first object.
-            closure(web_sys::MouseEvent::new("mock")?);
-            let closure = Closure::<dyn FnMut(_)>::new(closure);
-            run_button
-                .add_event_listener_with_callback("click", closure.as_ref().unchecked_ref())?;
-            closure.forget();
-        }
-        Ok(ScriptUI {})
+        let ui = Rc::new(RefCell::new(ScriptUI { primitive: None }));
+        register_run_callback(&ui, &code_area, &run_button, engine, on_new_object_callback)?;
+        register_mesh_callback(&ui, &output_area, &mesh_button)?;
+        Ok(ui)
     }
+}
+
+fn connect_output_to_engine<E: ScriptEngine + 'static>(
+    output_area: &HtmlTextAreaElement,
+    engine: &mut E,
+) {
+    let output_area = output_area.clone();
+    engine.on_print(move |s| {
+        let mut output = output_area.value();
+        output.push_str(s);
+        output.push('\n');
+        output_area.set_value(&output);
+    });
+}
+
+fn register_run_callback<E: ScriptEngine + 'static>(
+    ui: &Rc<RefCell<ScriptUI>>,
+    code_area: &HtmlTextAreaElement,
+    run_button: &HtmlButtonElement,
+    engine: E,
+    on_new_object_callback: impl Fn(&(dyn Primitive)) + 'static,
+) -> Result<(), JsValue> {
+    let code_area = code_area.clone();
+    let ui = ui.clone();
+    let closure = move |_event: web_sys::MouseEvent| match engine.eval(&code_area.value()) {
+        Ok(primitive) => {
+            ui.borrow_mut().primitive = Some(primitive.clone());
+            on_new_object_callback(&*primitive)
+        }
+        Err(e) => info!("was err: {:?}", e),
+    };
+    // Run closure once to create first object.
+    closure(web_sys::MouseEvent::new("mock")?);
+    let closure = Closure::<dyn FnMut(_)>::new(closure);
+    run_button.add_event_listener_with_callback("click", closure.as_ref().unchecked_ref())?;
+    closure.forget();
+    Ok(())
+}
+
+fn register_mesh_callback(
+    ui: &Rc<RefCell<ScriptUI>>,
+    output_area: &HtmlTextAreaElement,
+    mesh_button: &HtmlButtonElement,
+) -> Result<(), JsValue> {
+    let ui = ui.clone();
+    let output_area = output_area.clone();
+    let closure = move |_event: web_sys::MouseEvent| match &ui.borrow().primitive {
+        Some(p) => {
+            let (vertices, indices) = mesh_primitive(p);
+            let mut output = output_area.value();
+            output.push_str(&format!(
+                "Got {} vertices and {} triangles.\n",
+                vertices.len() / 6,
+                indices.len() / 3
+            ));
+            output_area.set_value(&output);
+        }
+        None => {
+            let mut output = output_area.value();
+            output.push_str("No object found. (Click Run first?)\n");
+            output_area.set_value(&output);
+        }
+    };
+    let closure = Closure::<dyn FnMut(_)>::new(closure);
+    mesh_button.add_event_listener_with_callback("click", closure.as_ref().unchecked_ref())?;
+    closure.forget();
+    Ok(())
+}
+
+pub struct PrimitiveSource {
+    pub primitive: Box<dyn Primitive>,
+}
+
+impl isosurface::source::Source for PrimitiveSource {
+    fn sample(&self, x: f32, y: f32, z: f32) -> f32 {
+        match self.primitive.eval(na::Vector3::new(x, y, z)) {
+            Ok(d) => d,
+            Err(_) => f32::NAN,
+        }
+    }
+}
+
+fn mesh_primitive(p: &Box<dyn Primitive>) -> (Vec<f32>, Vec<u32>) {
+    let source = Box::new(PrimitiveSource {
+        primitive: p.clone(),
+    });
+    let source = isosurface::source::CentralDifference::new(source);
+    let mut marching_cubes =
+        isosurface::linear_hashed_marching_cubes::LinearHashedMarchingCubes::new(3);
+    let mut vertices = Vec::new();
+    let mut indices = Vec::new();
+    marching_cubes.extract_with_normals(&source, &mut vertices, &mut indices);
+    (vertices, indices)
 }
